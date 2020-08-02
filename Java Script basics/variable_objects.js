8cc6cb6978ff861f85349e20a583e49f9ed1682d
Chander Bhanu <chanderbhanu096@gmail.com>
2020-08-02 21:59:20 +0530

    first commit

---
+++ Java Script basics/variable_objects.js
@@ -0,0 +1,14 @@
+// Variables
+const numbervariable = 10
+const stringvariable = "bhanu"
+const booleanvariable = true
+const nullvariable = null
+
+
+console.log(numbervariable)
+console.log(stringvariable)
+console.log(booleanvariable)
+console.log("10") //how to print nimber as a string
+
+// type casting 
+console.log(numbervariable+"") // converting from number to string 
